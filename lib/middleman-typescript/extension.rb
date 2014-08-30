380b3c811187d9082493015aaf49256830db9a16
patorash <chariderpato@gmail.com>
2014-08-31 04:00:34 +0900

    --noImplicitAny option

--- lib/middleman-typescript/extension.rb
+++ lib/middleman-typescript/extension.rb
@@ -4,17 +4,23 @@ require 'pry'
 module Middleman
   class TypescriptExtension < Extension
     option :typescript_dir, 'typescripts', 'Set TypeScript dir.'
+    option :target, 'ES5', 'Target version.(Default ES5)'
+    option :no_implicit_any, true, 'Use --noImplicitAny option.(Default true)'
     def initialize(app, options_hash={}, &block)
       super
-      app.set :typescript_dir, options.typescript_dir
       return unless app.environment == :development
+
+      app.set :typescript_dir, options.typescript_dir
+      compile_options = ['--target', options.target]
+      compile_options << '--noImplicitAny' if options.no_implicit_any
+      app.set :typescript_compile_options, compile_options
 
       app.ready do
         files.changed do |file|
           next if File.extname(file) != '.ts'
 
           file_path = "#{Dir.pwd}/#{file}"
-          result = TypeScript::Node.compile_file(file_path, '--target', 'ES5')
+          result = TypeScript::Node.compile_file(file_path, *app.typescript_compile_options)
           if result.success?
             file_name = File.basename(file_path).gsub(/\.ts/, '.js')
             export_dir = source_dir + File.dirname(file_path).sub(source_dir, '').sub(app.typescript_dir, app.js_dir)
